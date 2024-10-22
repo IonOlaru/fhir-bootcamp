fccced39b0d738c4e8b20e77f42eb6e89b2cbbf8
Ion Olaru <ion@olaru.us>
2024-10-22 10:07:00 -0400

    Patient search

--- 01-patient-management-app/src/main/java/com/luminatehealth/fhir/client/LiteFhirClient.java
+++ 01-patient-management-app/src/main/java/com/luminatehealth/fhir/client/LiteFhirClient.java
@@ -19,7 +19,7 @@ public abstract class LiteFhirClient<T extends DomainResource> {
     public static final String DEFAULT_FHIR_SERVER = "https://hapi.fhir.org/baseR4";
     public static final int FHIR_CLIENT_TIMEOUT = 5000;
 
-    private final IGenericClient iGenericClient;
+    protected final IGenericClient iGenericClient;
 
     public static final Map<Class, String> RESOURCE_TYPE_MAP = new HashMap<>();
 
@@ -54,6 +54,13 @@ public abstract class LiteFhirClient<T extends DomainResource> {
         IIdType idType = new IdType(RESOURCE_TYPE_MAP.get(t.getClass()), id);
         t.setId(idType.toString());
         iGenericClient.update().resource(t).execute();
+        return t;
+    }
+
+    public T delete(String id, T t) {
+        IIdType idType = new IdType(RESOURCE_TYPE_MAP.get(t.getClass()), id);
+        t.setId(idType.toString());
+        iGenericClient.delete().resource(t).execute();
         return t;
     }
 
