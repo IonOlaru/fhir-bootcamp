48f7d11783dbf4fc63cd14ca794ce643752ff448
Ion Olaru <ion@olaru.us>
2024-11-10 21:00:54 -0500

    Add EPIC patient info view

--- 01-patient-management-app/src/main/java/com/luminatehealth/fhir/client/EpicFhirClient.java
+++ 01-patient-management-app/src/main/java/com/luminatehealth/fhir/client/EpicFhirClient.java
@@ -5,13 +5,23 @@ import org.hl7.fhir.r4.model.DomainResource;
 
 public class EpicFhirClient<T extends DomainResource> extends LiteFhirClient<T> {
 
-    public EpicFhirClient() {}
+    public EpicFhirClient() {
+    }
 
     public EpicFhirClient(String baseUrl, Integer timeout) {
         super(baseUrl, timeout);
     }
 
+    /**
+     * THIS CODE DOES NOT SUPPORT BEING USED BY MULTIPLE BROWSER SESSIONS AT THE SAME TIME
+     * CHANGES ARE NEEDED TO ALLOW DIFFERENT SESSIONS TO USE THEIR OWN `bearerToken`
+     */
     public void registerAuthInterceptor(String bearerToken) {
+        iGenericClient.getInterceptorService().getAllRegisteredInterceptors().stream()
+                .filter(interceptor -> interceptor instanceof BearerTokenAuthInterceptor)
+                .findFirst()
+                .ifPresent(iGenericClient.getInterceptorService()::unregisterInterceptor);
+
         iGenericClient.registerInterceptor(new BearerTokenAuthInterceptor(bearerToken));
     }
 }
