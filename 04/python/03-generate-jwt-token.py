1d093cee22db7d926321bc43f2828220713c1f8f
Ion Olaru <ion@olaru.us>
2024-12-19 01:06:28 -0500

    fhir bulk request api

--- 04/python/03-generate-jwt-token.py
+++ 04/python/03-generate-jwt-token.py
@@ -2,6 +2,8 @@ import base64
 import json
 import time
 import uuid
+import requests
+
 
 import jwt
 from cryptography.hazmat.backends import default_backend
@@ -9,7 +11,10 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 
 
 token_url = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token"
-client_id = "<PLACE_HOLDER>"
+client_id = "<PLACEHOLDERS>"
+group_id = "<PLACEHOLDERS>"
+fhir_base_url = "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4"
+bulk_request_url = f"{fhir_base_url}/Group/{group_id}/$export"
 
 # Function to decode base64url to bytes
 def base64url_decode(base64url):
@@ -76,3 +81,49 @@ payload = {
 
 token = jwt.encode(payload, private_key, algorithm='RS384')
 print(f"JWT: {token}")
+print()
+
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+data = {
+    'grant_type': 'client_credentials',
+    'client_assertion_type': 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+    'client_assertion': token
+}
+
+response = requests.post(url=token_url, headers=headers, data=data).json()
+access_token = response['access_token']
+print(f"access_token: {access_token}")
+print()
+
+# initiate bulk request
+bulk_init_headers = {
+    'Authorization': f"Bearer {access_token}",
+    'Accept': 'application/fhir+json',
+    'Prefer': 'respond-async'
+}
+
+bulk_init_response = requests.get(url=bulk_request_url, headers=bulk_init_headers)
+
+if bulk_init_response.status_code == 202:
+    location_url = bulk_init_response.headers.get('Content-Location')
+    if location_url:
+        print(f"Polling URL: {location_url}")
+
+        while True:
+            poll_response = requests.get(location_url)
+            if poll_response.status_code == 200:
+                print("Received 200 OK. Here is the JSON response:")
+                print(poll_response.json())
+                break
+            else:
+                print(f"Waiting... Current status: {poll_response.status_code}")
+                time.sleep(5)
+    else:
+        print("Content-Location header is missing in the response.")
+
+else:
+    print(f"Initial request failed with status: {bulk_init_response.status_code}")
+    print(f"body: {json.dumps(json.loads(bulk_init_response.text), indent=4)}")
